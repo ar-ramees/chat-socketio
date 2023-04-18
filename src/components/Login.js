69ee0f9e50ea53bcda827c83cd381aba6e8fe5f0
Ramees Ahmed <rameesahmed6@gmail.com>
2023-04-18 13:06:42 +0530

    login n create id button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid'
 
-export default function Login() {
+export default function Login({onIdSubmit}) {
+    const idRef = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        onIdSubmit(idRef.current.value)
+    }
+
+    const createNewId = () => {
+        onIdSubmit(uuidV4())
+    }
+
+
   return (
-    <div>Login</div>
+    <Container className='align-items-center d-flex' style={{height: '100vh'}}>
+        <Form onSubmit={handleSubmit} className='w-100'>
+            <Form.Group>
+                <Form.Label>Enter Your ID</Form.Label>
+                <Form.Control type='text' ref={idRef} required />
+            </Form.Group>
+            <Button  type="submit" className="m-2">Login</Button>
+            <Button  onClick={createNewId} variant="secondary">Create A New Id</Button>
+        </Form>
+    </Container>
   )
 }
