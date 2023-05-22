40bd281fdac8e998d3918a5947abb4896a316953
Ramees Ahmed <rameesahmed6@gmail.com>
2023-05-23 03:04:06 +0530

    chat scrolling callback function

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function OpenConversation() {
     const [text, setText] = useState('')
+    // const lastMessageRef = useRef()
+    const setRef = useCallback(node => {
+        if (node) {
+            node.scrollIntoView({ smooth: true })
+        }
+    }, [])
+
     const { sendMessage, selectedConversation } = useConversations()
 
     function handleSubmit(e) {
@@ -16,15 +23,24 @@ export default function OpenConversation() {
         setText('')
     }
 
+    // useEffect(() => {
+    //     if(lastMessageRef.current) {
+    //         lastMessageRef.current.scrollIntoView({ smooth: true })
+    //     }
+    // }, [lastMessageRef.current])
+
   return (
     <div className="d-flex flex-column flex-grow-1">
         <div className='flex-grow-1 overflow-auto'>
-            <div className='h-100 d-flex flex-column align-items-start justify-content-end px-3'>
+            <div className='d-flex flex-column align-items-start justify-content-end px-3'>
                 {selectedConversation.messages.map((message, index) => {
+                    const lastMessage = selectedConversation.messages.length - 1 === index
                     return (
                         <div
+                            ref = {lastMessage ? setRef : null}
                             key={index}
-                            className='my-1 d-flex flex-column'
+                            className={`my-1 d-flex flex-column
+                            ${message.fromMe ? 'align-self-end' : ''}`}
                         >
                             <div
                                 className={`rounded px-2 py-1 ${message.fromMe 
